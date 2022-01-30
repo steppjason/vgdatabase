5da7901c603e7b9eb465140786f43c52022c05a1
Jason Stepp <hi@jasonstepp.me>
2022-01-29 17:50:50 -0700

    Refactor
    Turned gameService into a class to make code look cleaner.

--- client/src/services/gameService.js
+++ client/src/services/gameService.js
@@ -23,27 +23,3 @@ class GameService{
 }
 
 export default GameService
-
-// function  getGames() {
-//     return api().get('/')
-// }
-
-// function getGame(id) {
-//     return api().get(`/${id}`)
-// }
-
-// function addGame(game) {
-//     return api().post(`/${game}`)
-// }
-
-// function updateGame(id) {
-//     return api().put(`/${id}`)
-// }
-
-// function deleteGame(id) {
-//     return api().delete(`/${id}`)
-// }
-
-//export default { getGames, getGame, addGame, updateGame, deleteGame }
-
-
